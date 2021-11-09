0d29aa2cb9ca74fde57c586461fc8101d7e813e2
wuchuheng <root@wuchuheng.com>
2021-11-09 18:32:45 +0800

    feat: Initialize the feature for deploded

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+localDir=build
+remoteDir=/www/wwwroot/wuchuheng.com
+yarn build
+
+sftp root@demo.jds.wuchuheng.com <<EOF
+  cd $remoteDir
+  lcd $localDir
+  put -r * ./
+EOF
+
+docker run -it --env-file=.env -e "CONFIG=$(cat ./config.json | jq -r tostring)" algolia/docsearch-scraper
