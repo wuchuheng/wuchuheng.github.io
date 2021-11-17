36a9573d770c8f7edab2a305427dbf99f83fb076
wuchuheng <root@wuchuheng.com>
2021-11-18 03:47:17 +0800

    docs: updated 《Graphql.SpringBoot架构GraphQL.序言》

--- src/components/CodesanboxRender/CodesanboxPurePreviewRender/index.tsx
+++ src/components/CodesanboxRender/CodesanboxPurePreviewRender/index.tsx
@@ -6,19 +6,22 @@ interface CodesanboxSimplePreviewRenderProps {
     url: string
 }
 const CodesanboxPurePreviewRender = ({url}: CodesanboxSimplePreviewRenderProps): React.ReactElement => {
-    const data =
-        [
+    const data = [
             {
-                id: 1,
-                name: "banner1",
-                url: "http:xxx.com/xxx"
+                "uid": "1",
+                "title": "狂人日记",
+                "id": "1",
+                "chapters": [
+                    {
+                        "id": "1",
+                        "title": "狂人日记",
+                        "content": "虚..., 别出声,他们在吃人..."
+                    }
+                ],
+                "updatedTime": "2021-11-17T11:56:05.106Z"
             },
-            {
-                id: 2,
-                name: "banner2",
-                url: "http:xxx.com/xxx"
-            }
-        ]
+        ];
+
     const html = prettyPrintJson.toHtml(data)
 return (
     <>
