8c3d498762b5fd331b9908df46cdab10cf0fb5fe
wuchuheng <root@wuchuheng.com>
2022-01-08 22:59:51 +0800

    docs: created 《算法.防抖和节流算法》

--- src/components/IframeContainer/Index.tsx
+++ src/components/IframeContainer/Index.tsx
@@ -4,22 +4,60 @@ import LoadingSpinner from "../LoadingSpinner";
 interface IframeContainerProps {
     src: string;
     style: {[key: string]: string}
+    codesandbox?: boolean
 }
-const IframeContainer = ({src, style}: IframeContainerProps): React.ReactElement => {
+const IframeContainer = ({src, style, ...props}: IframeContainerProps): React.ReactElement => {
     const [showLoading, setShowLoading] = useState<boolean>(true)
     const handleOnload = () => setShowLoading(false )
+    const codesandboxAttr = {
+        sandbox: "allow-modals allow-forms allow-popups allow-scripts allow-same-origin",
+        allow: "accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    }
+    const isCodesandboxAttr = src.match(/codesandbox\.io/) != null
+    console.log(isCodesandboxAttr)
+    const CodesandboxIframe = (): React.ReactElement => {
+        return (
+            <>
+                <iframe
+                    src={src}
+                    style={{
+                        width: "100%",
+                        height: "500px",
+                        border: "0",
+                        borderRadius: "4px",
+                        overflow: "hidden"
+                    }}
+                    sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+                    onLoad={handleOnload}
+                    {...(isCodesandboxAttr ? codesandboxAttr : {})}
+                />
+                {showLoading && <LoadingSpinner style={{...style}}/>}
+            </>
+        )
+    }
+    const DefaultIframe = () => {
+        return (
+            <>
+                <iframe
+                    src={src}
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        ...style,
+                        display: showLoading ? 'none' : 'block'}}
+                    onLoad={handleOnload}
+                    {...(isCodesandboxAttr ? codesandboxAttr : {})}
+                />
+                {showLoading && <LoadingSpinner style={{...style}}/>}
+            </>
+        )
+    }
+
+
     return (
         <>
-            <iframe
-                src={src}
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    ...style,
-                    display: showLoading ? 'none' : 'block'}}
-                onLoad={handleOnload}
-            />
-            {showLoading && <LoadingSpinner style={{...style}}/>}
+            {isCodesandboxAttr && <CodesandboxIframe />}
+            {!isCodesandboxAttr && <DefaultIframe />}
         </>
     )
 
