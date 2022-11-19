153da23fbf2518c8c4f1b24a261a29d06e626d19
wuchuheng <root@wuchuheng.com>
2022-11-19 20:50:49 +0800

    docs: created docs/08.JS/03.CSS/3.划一条弧线背景.mdx .

---
+++ src/components/Curvedbackground/Curvedbackground.tsx
@@ -0,0 +1,16 @@
+/**
+ *  This file is part of emailNotes.
+ *
+ * @Description Say something for this file.
+ * @Author      wuchuheng<root@wuchuheng.com>
+ * @Time        2022/11/19 20:38
+ */
+import React from "react";
+import style from "./style.module.scss"
+
+const Curvedbackground: React.FC = () => {
+    return (<div className={style.main}/>)
+
+}
+
+export default  Curvedbackground
