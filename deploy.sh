6be9d8bc2f28884f43cce404e3b64d8584f3f646
wuchuheng <root@wuchuheng.com>
2025-12-19 03:04:19 +0800

    refactor: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 localDir=build
-remoteDir=/root/myProjects/linux_server_tools/www/wwwroot/wuchuheng.com
+remoteDir=/opt/1panel/apps/openresty/openresty/www/sites/wuchuheng.com/index
 pnpm build
 
-if [ $? !eq 0 ]; then
+if [ $? -ne 0 ]; then
   RED='\033[0;31m'
   echo -e "❌ ❌ ❌ ${RED} React build failure."
     if command -v notificationTool-darwin-amd64 &> /dev/null
@@ -12,34 +12,48 @@ if [ $? !eq 0 ]; then
         notificationTool-darwin-amd64 "The notes failed to be build."
     fi
 
-  exit
+  exit 1
 fi
 
-echo $?
+echo "Build successful."
 
 tar -zcvf build.tar.gz build
 
-sftp root@ys.wuchuheng.com <<EOF
+sftp root@tc.wuchuheng.com <<EOF
   cd $remoteDir
   put build.tar.gz
   pwd
   ls -ahl
 EOF
 
-ssh root@ys.wuchuheng.com <<EOF
+ssh root@tc.wuchuheng.com <<EOF
   cd $remoteDir
   pwd
   mv build.tar.gz ~/build.tar.gz
   rm -rf ./*
   mv ~/build.tar.gz ./
   tar -zxvf build.tar.gz
-  mv build/* build/.* ./
+  cp -a build/. .
   rm -rf build
   rm -rf build.tar.gz
 EOF
 
 rm build.tar.gz
 
-docker run -it --env-file=.env -e "CONFIG=$(json5 ./config.json5)" --name crawler algolia/docsearch-scraper
+if command -v docker &> /dev/null; then
+  CONFIG_JSON=""
+  if command -v json5 &> /dev/null; then
+    CONFIG_JSON=$(json5 ./config.json5)
+  elif command -v npx &> /dev/null; then
+    CONFIG_JSON=$(npx -y json5 ./config.json5)
+  fi
 
-docker rm crawler
+  if [ -n "$CONFIG_JSON" ]; then
+    docker run -it --env-file=.env -e "CONFIG=$CONFIG_JSON" --name crawler algolia/docsearch-scraper
+    docker rm crawler
+  else
+    echo "Warning: json5 command not found. Skipping Algolia scraper."
+  fi
+else
+  echo "Warning: Docker not found. Skipping Algolia scraper."
+fi
--- src/components/HomeFooterRender/index.tsx
+++ src/components/HomeFooterRender/index.tsx
@@ -5,7 +5,7 @@ const HomeFooterRender = (): React.ReactElement => {
     return (
         <div className={styles.mainWrapper}>
             <p>
-                © 2017 - {(new Date).getFullYear()} Wuchuheng. All Rights Reserved. &emsp; <a href="https://beian.miit.gov.cn/">粤ICP备17071471号-2</a>
+                © 2017 - {(new Date).getFullYear()} Wuchuheng. All Rights Reserved. 
             </p>
         </div>
     )
